4c32608d09f0449d23bfa7a9b6db304ebbb407b3
Chan Xue Li <112333943+chanxueli@users.noreply.github.com>
2023-08-01 17:41:55 +0100

    transfer from team repo

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# Alexander Thomson's Special Collection Showcase (Team 4A Journey)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+Welcome to our static website showcasing the journey of our team in developing a website dedicated to presenting Alexander Thomson's extraordinary collection.
+
+Access our static website here: https://whathellahor.github.io/3003-uog/
+
+## Problem Statement
+We aim to showcase Alexander Thomson's special collection, housed at the Mitchell Library, to a wider audience and raise awareness about his significant architectural contributions.
+
+## Solution
+The team has developed a website to showcase the exceptional collection of Alexander Thomson, the revered architect, held at the Mitchell Library.
+The website boasts several captivating features, providing users with an immersive and educational experience:
+
+1. **Biography Page:** Delve into the life and accomplishments of Alexander Thomson, gaining insights into his extraordinary journey as an architect.
+
+2. **Collections Page:** Explore a gallery of Alexander Thomson's exquisite architectural drawings, offering a glimpse into the essence of his iconic designs.
+
+3. **Individual Collection Page:** Engage with specific drawings, equipped with cutting-edge technologies such as AR, Google Map 360 Street View, and text-to-speech, allowing users to delve deeper into the intricacies of each masterpiece. Users can share their discoveries on social media and express their thoughts.
+
+4. **Virtual Tour Page:** Embark on curated tours to virtually visit Alexander Thomson's iconic buildings scattered across Glasgow, experiencing the magnificence of his works firsthand.
+
+5. **Contact Us Page:** For any inquiries or additional information, users can easily reach out to us through the Contact Us page.
+
+6. **Social Media Page:** Stay updated with the latest posts related to Alexander Thomson on Instagram, Facebook, and Twitter, connecting with fellow enthusiasts and architecture aficionados.
+
+View our solution website here: https://whathellahor.github.io/3003-alexanderthomson/
+
+# How to Use Our Static Website
+
+| Section  | Description |
+| ------------- | ------------- |
+| `Main Page` | An introduction to Team 4A's static website |
+| `Problem` | Explains our clients problem statement with context and project brief attached. |
+| `Exploration` | Our dailies also known as our daily dairies as we reflect on what we have learnt and done every working day. |
+| `Chosen Idea` | Highlights our solutions's functionalities, features and technology used to develop the website. |
+| `Project Process` | A very detailed documentation on the team's development process. This includes pre-development, planning and final solution. |
+| `Reflection` | Highlights our key takeaways and reflection everyday. |
+| `Proud` | Highlights our key achievements. |
---
+++ script.js
@@ -0,0 +1,113 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+let caledoniaSlideIndex = 1;
+let buckHeadSlideIndex = 1;
+let cecilStreetSlideIndex = 1;
+let ethonSlideIndex = 1;
+let greatWesternIndex = 1;
+let lilybankIndex = 1;
+let sirRobertIndex = 1;
+let sixtyIndex = 1;
+let eglintonIndex = 1;
+
+showSlides(caledoniaSlideIndex, "caledonia");
+showSlides(buckHeadSlideIndex, "buckHead");
+showSlides(cecilStreetSlideIndex, "cecilStreet");
+showSlides(ethonSlideIndex, "ethon");
+showSlides(greatWesternIndex, "greatWestern");
+showSlides(lilybankIndex, "lilybank");
+showSlides(sirRobertIndex, "sirRobert");
+showSlides(sixtyIndex, "sixty");
+showSlides(eglintonIndex, "eglinton");
+
+// function plusSlides(n, buildingName) {
+//   if (buildingName === "caledonia") {
+//     showSlides((caledoniaSlideIndex += n), "caledonia");
+//   } else if (buildingName === "buckHouse") {
+//     showSlides((buckHouseSlideIndex += n), "buckHouse");
+//   }
+// }
+
+function currentSlide(n, buildingName) {
+  if (buildingName === "caledonia") {
+    showSlides((caledoniaSlideIndex = n), "caledonia");
+  } else if (buildingName === "buckHead") {
+    showSlides((buckHeadSlideIndex = n), "buckHead");
+  } else if (buildingName === "cecilStreet") {
+    showSlides((cecilStreetSlideIndex = n), "cecilStreet");
+  } else if (buildingName === "ethon") {
+    showSlides((ethonSlideIndex = n), "ethon");
+  } else if (buildingName === "greatWestern") {
+    showSlides((greatWesternIndex = n), "greatWestern");
+  } else if (buildingName === "lilybank") {
+    showSlides((lilybankIndex = n), "lilybank");
+  } else if (buildingName === "sirRobert") {
+    showSlides((sirRobertIndex = n), "sirRobert");
+  } else if (buildingName === "sixty") {
+    showSlides((sixtyIndex = n), "sixty");
+  } else if (buildingName === "eglinton") {
+    showSlides((eglintonIndex = n), "eglinton");
+  }
+}
+
+function showSlides(n, buildingName) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides " + buildingName);
+  let dots = document.getElementsByClassName("dot " + buildingName);
+  if (n > slides.length) {
+    if (buildingName === "caledonia") {
+      caledoniaSlideIndex = 1;
+    } else if (buildingName === "buckHead") {
+      buckHeadSlideIndex = 1;
+    } else if (buildingName === "cecilStreet") {
+      cecilStreetSlideIndex = 1;
+    } else if (buildingName === "ethon") {
+      ethonSlideIndex = 1;
+    } else if (buildingName === "greatWestern") {
+      greatWesternIndex = 1;
+    } else if (buildingName === "lilybank") {
+      lilybankIndex = 1;
+    } else if (buildingName === "sirRobert") {
+      sirRobertIndex = 1;
+    } else if (buildingName === "sixty") {
+      sixtyIndex = 1;
+    } else if (buildingName === "eglinton") {
+      eglintonIndex = 1;
+    }
+  }
+  if (n < 1) {
+    if (buildingName === "caledonia") {
+      caledoniaSlideIndex = slides.length;
+    } else if (buildingName === "buckHead") {
+      buckHeadSlideIndex = slides.length;
+    } else if (buildingName === "cecilStreet") {
+      cecilStreetSlideIndex = slides.length;
+    } else if (buildingName === "ethon") {
+      ethonSlideIndex = slides.length;
+    } else if (buildingName === "greatWestern") {
+      greatWesternIndex = slides.length;
+    } else if (buildingName === "lilybank") {
+      lilybankIndex = slides.length;
+    } else if (buildingName === "sirRobert") {
+      sirRobertIndex = slides.length;
+    } else if (buildingName === "sixty") {
+      sixtyIndex = slides.length;
+    } else if (buildingName === "eglinton") {
+      eglintonIndex = slides.length;
+    }
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[n - 1].style.display = "block";
+  dots[n - 1].className += " active";
+}
